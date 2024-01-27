fd0ab235cc463e11ea0f31be393954e02edfb298
Ap0ll1n <symkaz29@gmail.com>
2024-01-27 18:21:52 +0100

    asdasdasd

--- Assets/Scripts/FollowPoznanski.cs
+++ Assets/Scripts/FollowPoznanski.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class FollowPoznanski : MonoBehaviour
 {
+    [SerializeField] private Catapult catapult;
     [SerializeField] private Transform poznanski;
     [SerializeField] private Vector3 lookVector = new(-1, 1, 0);
     [SerializeField] private float distance = 10;
+
     void Update()
     {
-        transform.position = poznanski.position + lookVector * distance;
-        transform.LookAt(poznanski);
+        //transform.position = poznanski.position + lookVector * distance;
+        //transform.LookAt(poznanski);
+        transform.position=poznanski.position;
+
+        if(catapult.GetIsLunched())
+        {
+            foreach (Transform child in transform)
+            {
+                Debug.Log("activate vfx");
+                child.gameObject.SetActive(true);
+            }
+        }
     }
 }
